df53d93f1ae89b82dbf6174c45d62fbf71918c43
ilabstaff <ilabstaff@MXTechiMac82461.local>
2019-02-06 17:57:41 -0800

    greeter

---
+++ src/greeter.java
@@ -0,0 +1,8 @@
+import javax.swing.JOptionPane;
+
+public class greeter {
+public static void main(String[] args) {
+	String Rob = JOptionPane.showInputDialog("what is you name");
+	JOptionPane.showMessageDialog(null,"hello " + Rob  );
+}
+}
--- src/strings_and_dialogs/RobotSquare.java
+++ src/strings_and_dialogs/RobotSquare.java
@@ -4,12 +4,32 @@ package strings_and_dialogs;
  *    Level 0
  */
 
+import javax.swing.JOptionPane;
+
 import org.jointheleague.graphical.robot.Robot;
 
 // This recipe draws a square using the Robot
 
 public class RobotSquare {
     public static void main(String[] args) throws Exception {
+    	
+    	Robot jama = new Robot();
+    	jama.turn(90);
+    	String hashi = JOptionPane.showInputDialog("sides");
+    	int Slide= Integer.parseInt(hashi);
+    	for (int i = 0; i < Slide ; i++) { 
+    	    jama.setSpeed(15);
+    	    jama.penDown();
+    		jama.move(80);
+    		jama.turn(360/Slide);
+    		
+    	
+   
+    
+    	}
+    
+    
+    	
  
     	// 1. Make a new Robot
 
